9ecff3d47dfb82a8e459f5a48b41ee69b1772916
ggarcia23 <89540967+ggarcia23@users.noreply.github.com>
2021-11-09 19:25:15 -0500

    Add files via upload

---
+++ hash_map.py
@@ -0,0 +1,16 @@
+import unittest
+
+"""insert hash map stuff here I guess..."""
+
+
+class TestClass(unittest.TestCase):
+    """insert tests here"""
+    pass
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
