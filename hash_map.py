88b07eb5ab273ed53b996f921b0d74cb7628e64d
wendallR <wendallar125@gmail.com>
2021-11-29 15:20:21 -0500

    Turn Book's sudo-code into real python code

--- hash_map.py
+++ hash_map.py
@@ -1,7 +1,37 @@
 import unittest
+import math
 
 """insert hash map stuff here I guess..."""
 
+class HashTable():
+
+    innerDictionaryForHashTable = dict()
+
+    def __init__(self, size):
+        self.size = size
+
+    def insert(self,key):
+
+        for index in range(self.size):
+            hashedPosition = self.hashKey(key,index)
+            if self.innerDictionaryForHashTable[hashedPosition] == None:
+                self.innerDictionaryForHashTable[hashedPosition] = key
+                return None
+
+    def search(self,key):
+        for index in range(self.size):
+            hashedPosition = self.hashKey(key,index)
+            if self.innerDictionaryForHashTable[hashedPosition] == key:
+                return hashedPosition
+            if self.innerDictionaryForHashTable[hashedPosition] == None:
+                return None
+
+    def hashKey(self, key, index):
+        return (self.auxiliaryHash(key) + index) % self.size
+
+    def auxiliaryHash(self, key):
+        hashingNumber = (math.sqrt(5)-1)/2
+        return math.floor(self.size * ( key * hashingNumber ) % 1 )
 
 class TestClass(unittest.TestCase):
     """insert tests here"""
