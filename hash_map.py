ac7ac0c50a0b8ffccc29cdd4a17bbf993d63f283
wendallR <wendallar125@gmail.com>
2021-11-30 15:07:14 -0500

    Fix insert and reformat insert and test

--- hash_map.py
+++ hash_map.py
@@ -5,39 +5,43 @@ import math
 
 class HashTable():
 
-    innerDictionaryForHashTable = dict()
 
     def __init__(self, size):
         self.size = size
+        self.innerList = [None for x in range(size)]
+
 
     def insert(self,key):
 
         for index in range(self.size):
             hashedPosition = self.hashKey(key,index)
-            if self.innerDictionaryForHashTable[hashedPosition] == None:
-                self.innerDictionaryForHashTable[hashedPosition] = key
+            if not self.innerList[hashedPosition]:
+                self.innerList[hashedPosition] = key
                 return None
 
     def search(self, key):
         for index in range(self.size):
             hashedPosition = self.hashKey(key,index)
-            if self.innerDictionaryForHashTable[hashedPosition] == key:
+            if self.innerList[hashedPosition] == key:
                 return hashedPosition
-            if self.innerDictionaryForHashTable[hashedPosition] == None:
+            if self.innerList[hashedPosition] == None:
                 return None
 
     def hashKey(self, key, index):
         def auxiliaryHash():
             hashingNumber = (math.sqrt(5)-1)/2
-            return math.floor(self.size * (key * hashingNumber) % 1)
-
+            return math.floor(self.size * ((key * hashingNumber) % 1))
         return (auxiliaryHash() + index) % self.size
 
 
 
 class TestClass(unittest.TestCase):
     """insert tests here"""
-    pass
+    def test_insert_and_search_key_of_table_of_size_127(self):
+        testTable = HashTable(127)
+        testTable.insert(31313)
+        result = testTable.search(31313)
+        self.assertEqual(63,result)
 
 
 def main():
