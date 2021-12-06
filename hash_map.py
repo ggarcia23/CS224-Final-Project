4bab97951c8c4f35e01cc4b124074018416b77cd
wendallR <wendallar125@gmail.com>
2021-12-06 15:00:41 -0500

    Refactor Code to be proper to Python standards

--- hash_map.py
+++ hash_map.py
@@ -6,7 +6,7 @@ import unittest
 import math
 
 
-class HashTable():
+class HashTable:
     """
     size is the size of the hash table, being 127
     """
@@ -16,19 +16,20 @@ class HashTable():
         """
         :
         """
-        self.innerList = [None for x in range(self.size)]
+        self.innerList = [None for _ in range(self.size)]
 
     def insert(self, key):
-        """ Inserts a key given by the parameter and hashes it into in proper hashed position in the hash table of size 127
+        """ Inserts a key given by the parameter and hashes it into in proper
+        hashed position in the hash table of size 127
 
         :param key: element containing the key to be hashed
         :type key: int
         :return: None
         """
         for index in range(self.size):
-            hashedPosition = self.hashKey(key, index)
-            if not self.innerList[hashedPosition] or self.innerList[hashedPosition] == "DELETED":
-                self.innerList[hashedPosition] = key
+            hashed_position = self.hash_key(key, index)
+            if not self.innerList[hashed_position] or self.innerList[hashed_position] == "DELETED":
+                self.innerList[hashed_position] = key
                 return None
 
     def search(self, key):
@@ -41,21 +42,20 @@ class HashTable():
         :rtype: int
         """
         for index in range(self.size):
-            hashedPosition = self.hashKey(key, index)
-            if self.innerList[hashedPosition] == key:
-                return hashedPosition
-            if self.innerList[hashedPosition] == None:
+            hashed_position = self.hash_key(key, index)
+            if self.innerList[hashed_position] == key:
+                return hashed_position
+            if self.innerList[hashed_position] is None:
                 return None
 
     def delete(self, key):
         """ will search for key, and delete it from the hash table, freeing its position from the hash table
 
         """
-        keyToBeDeleted = self.search(key)
-        self.innerList[keyToBeDeleted] = "DELETED"
+        key_to_be_deleted = self.search(key)
+        self.innerList[key_to_be_deleted] = "DELETED"
 
-
-    def hashKey(self, key, index):
+    def hash_key(self, key, index):
         """ finds the hashing position of the desired key by calling an inner function and modding it by the size of
             the hash table, returning the hashed position of the key
 
@@ -67,16 +67,16 @@ class HashTable():
         :rtype: int
         """
 
-        def auxiliaryHash():
+        def auxiliary_hash():
             """
 
             :return: the floor of the result given by the hashing number
             :rtype: int
             """
-            hashingNumber = (math.sqrt(5) - 1) / 2
-            return math.floor(self.size * ((key * hashingNumber) % 1))
+            hashing_number = (math.sqrt(5) - 1) / 2
+            return math.floor(self.size * ((key * hashing_number) % 1))
 
-        return (auxiliaryHash() + index) % self.size
+        return (auxiliary_hash() + index) % self.size
 
 
 class TestClass(unittest.TestCase):
@@ -85,49 +85,55 @@ class TestClass(unittest.TestCase):
     """
 
     def test_insert_and_search_key_of_table_of_size_127(self):
-        testTable = HashTable()
-        testTable.insert(31313)
-        result = testTable.search(31313)
+        test_table = HashTable()
+        test_table.insert(31313)
+        result = test_table.search(31313)
         self.assertEqual(63, result)
 
     def test_insert_and_search_key_of_zero(self):
-        testTable = HashTable()
-        testTable.insert(0)
-        result = testTable.search(0)
+        test_table = HashTable()
+        test_table.insert(0)
+        result = test_table.search(0)
         self.assertEqual(0, result)
 
     def test_insert_two_numbers_and_search_key(self):
-        testTable = HashTable()
-        testTable.insert(23)
-        testTable.insert(463)
-        result = testTable.search(463)
+        test_table = HashTable()
+        test_table.insert(23)
+        test_table.insert(463)
+        result = test_table.search(463)
         self.assertEqual(19, result)
 
     def test_char_in_hashTable(self):
-        testTable = HashTable()
-        testTable.insert(ord('C'))
-        result = testTable.search(ord('C'))
-        self.assertEqual(51,result)
+        test_table = HashTable()
+        test_table.insert(ord('C'))
+        result = test_table.search(ord('C'))
+        self.assertEqual(51, result)
 
     def test_insert_and_search_with_multiple_values_inserted(self):
-        testTable = HashTable()
-        testTable.insert(48923432)
-        testTable.insert(2324353543)
-        testTable.insert(1233434556547674)
-        testTable.insert(23546566575476)
-        testTable.insert(65346456356436543636)
-        testTable.insert(31313)
-        result = testTable.search(31313)
+        test_table = HashTable()
+        test_table.insert(48923432)
+        test_table.insert(2324353543)
+        test_table.insert(1233434556547674)
+        test_table.insert(23546566575476)
+        test_table.insert(65346456356436543636)
+        test_table.insert(31313)
+        result = test_table.search(31313)
         self.assertEqual(63, result)
 
     def test_search_and_delete(self):
-        testTable = HashTable()
-        testTable.insert(31313)
-        testTable.delete(31313)
-        testTable.insert(31313)
-        result = testTable.search(31313)
+        test_table = HashTable()
+        test_table.insert(31313)
+        test_table.delete(31313)
+        test_table.insert(31313)
+        result = test_table.search(31313)
         self.assertEqual(63, result)
 
+    def test_delete(self):
+        test_table = HashTable()
+        test_table.insert(31313)
+        test_table.delete(31313)
+        result = test_table.search(31313)
+        self.assertEqual(None, result)
 
 
 def main():
